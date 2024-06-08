ae360501cf43a8cf0b0d9f88228c06ab33c820b4
Anurag <anuragmishra0521@gmail.com>
2024-06-08 22:39:28 +0530

    error fixed about section

--- components/About.tsx
+++ components/About.tsx
@@ -29,7 +29,7 @@ function About({}: Props) {
         viewport={{once:true}}
         src="https://i.postimg.cc/CLwnmNff/banner-01-jpg.png" className='-mb-28 md:mb-0 flex-shrink-0 w-24 h-24 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[400px]'/>
         <div className='space-y-8 px-0 md:px-8 flex items-center flex-col'>
-            <p className='text-base'>Greetings! I am <b style={{color:'#F7AB0A'}}>Anuarg Mishra</b>, I am a tech enthusiast pursuing a Bachelor of Technology in Computer Science Engineering. I have had an interest in technology from an early age and I am planning to pursue a career in software development or web development. Whether it's academic or extracurricular activities, I am always ready to learn and dive deep into new technology, regardless of the level of dedication required.<br /> <br/>
+            <p className='text-base'>Greetings! I am <b style={{color:'#F7AB0A'}}>Anuarg Mishra</b>, I am a tech enthusiast pursuing a Bachelor of Technology in Computer Science Engineering. I have had an interest in technology from an early age and I am planning to pursue a career in software development or web development. Whether it is academic or extracurricular activities, I am always ready to learn and dive deep into new technology, regardless of the level of dedication required.<br /> <br/>
                 Willing to give me an opportunity or feel like I really worth it, here is the resume you can take into consideration <SwipeDownAltIcon/></p>
             <a href='https://drive.google.com/uc?export=download&id=1j9F9u_gWVFeSNl-e-Hu8YXj8NlCLlrkA' > <button className='bg-[#ff9b20] p-2 text-lg hover:bg-[#ff9b20]/90 rounded-md text-gray-100'> <DownloadForOfflineIcon/> Download </button> </a>
         </div>
